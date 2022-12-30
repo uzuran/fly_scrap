6c987963b0902585f449a48b307c045d80b45339
uzuran <archloverlinux@gmail.com>
2022-12-30 13:36:12 +0100

    - First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import kivy
+from kivy.app import App
+
+from kivy.uix.widget import Widget
+
+class MyGrid(Widget):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    MyApp().run()
+
