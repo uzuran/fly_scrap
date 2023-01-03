18d77617f8223ab0106b99561bd9a16f47bb255f
Pomelo <p0m3l0pack@gmail.com>
2023-01-03 10:05:18 +0100

    reapir
    repair root

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import kivy
 from kivy.app import App
-<<<<<<< Updated upstream
-=======
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+
 
 
 
@@ -15,22 +14,14 @@ class MyLayout(Widget):
         name = self.ids.name_input.text
 
         self.ids.name_label.text = f"Hello {name} !"
->>>>>>> Stashed changes
 
-from kivy.uix.widget import Widget
 
-class MyGrid(Widget):
-    pass
 
 class MyApp(App):
     def build(self):
-<<<<<<< Updated upstream
-        return MyGrid()
-=======
        
         return MyLayout()
 
->>>>>>> Stashed changes
 
 
 
