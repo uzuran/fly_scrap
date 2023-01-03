165bdf722b140241d5e7eff0f21ab9964558b3aa
uzuran <archloverlinux@gmail.com>
2023-01-03 21:00:59 +0100

    - Try somthing that you try PomeloPack.

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
+# Here is important that you will import kivymd that you need install separate by, pip or something in your IDE.
+from kivymd.app import MDApp
+from kivy.lang import Builder
 
-
-class MyLayout(Widget):
-    def click_btn(self):
-        name = self.ids.name_input.text
-
-        self.ids.name_label.text = f"Hello {name} !"
-
-
-class MyApp(App):
+# Please tell me here why we use the MDApp ? Here we can speak about that on meeting.
+class MyApp(MDApp):
     def build(self):
-       
-        return MyLayout()
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return Builder.load_file("log.kv")
 
 
 if __name__ == "__main__":
