c6d7cad768448d62721755dd5b2ced0a326b24c4
uzuran <archloverlinux@gmail.com>
2023-01-03 19:35:20 +0100

    - Clean not used variable and imports.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import kivy
 from kivy.app import App
-from kivy.uix.image import Image
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+
 
 class MyLayout(Widget):
-    greeting = ObjectProperty(None)
+
 
     def click_btn(self):
         name = self.ids.name_input.text
@@ -13,12 +11,10 @@ class MyLayout(Widget):
         self.ids.name_label.text = f"Hello {name} !"
 
 
-
 class MyApp(App):
     def build(self):
 
         return MyLayout()
-
 
 
 if __name__ == "__main__":
