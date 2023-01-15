c1ddcf85f965efda577d1ecd1dab379eff28053d
uzuran <archloverlinux@gmail.com>
2023-01-15 18:19:20 +0100

    - Create function for move to the registration page.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 # Here is important that you will import kivymd that you need install separate by, pip or something in your IDE.
 from kivymd.app import MDApp
 from kivy.lang import Builder
-
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 # Please tell me here why we use the MDApp ? Here we can speak about that on meeting.
+
+class MenuScreen(Screen):
+    pass
+
+class RegistrationScreen(Screen):
+    pass
+
+
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name="menu"))
+sm.add_widget(RegistrationScreen(name="registration"))
 class MyApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
